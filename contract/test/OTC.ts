2269bc087c23164728a4a01b1c88b64c7be27b1e
voyager-gold <voyagergoldev@gmail.com>
2023-06-05 14:14:20 -0600

    update unit test

--- contract/test/OTC.ts
+++ contract/test/OTC.ts
@@ -2,10 +2,10 @@ import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { TestEnv } from "./types";
+import { TestEnv, Otc } from "./types";
 
 describe("Offer", function () {
-  const env: TestEnv = {} as TestEnv
+  const env: TestEnv = {} as TestEnv;
 
   before(async () => {
     const [owner, ...accounts] = await ethers.getSigners();
@@ -15,117 +15,68 @@ describe("Offer", function () {
     const Otc = await ethers.getContractFactory("OTC");
     const Erc20 = await ethers.getContractFactory("ERC20Mock");
 
-    const offerFactory = await OfferFactory.deploy()
+    const offerFactory = await OfferFactory.deploy();
     const domainContract = await DomainContract.deploy();
-    const otc = await Otc.deploy(domainContract.address, offerFactory.address)
+    const otc = await Otc.deploy(domainContract.address, offerFactory.address);
 
-    const sa1 = await Erc20.deploy("SrcAsset1", "SrcAsset2")
-    const sa2 = await Erc20.deploy("SrcAsset2", "SrcAsset2")
-    const da1 = await Erc20.deploy("DestAsset1", "DestAsset1")
+    const sa1 = await Erc20.deploy("SrcAsset1", "SrcAsset2");
+    const sa2 = await Erc20.deploy("SrcAsset2", "SrcAsset2");
+    const da1 = await Erc20.deploy("DestAsset1", "DestAsset1");
 
-    env.otc = otc,
-    env.sourceAssets = [sa1, sa2],
-    env.destAssets = [da1],
-    env.owner = owner
-    env.accounts = accounts
-  })
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+    env.otc = otc;
+    env.srcAssets = [sa1, sa2];
+    env.destAssets = [da1];
+    env.owner = owner;
+    env.accounts = accounts;
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("OTC.addAsset", () => {
+    it("success", async () => {
+      const {
+        owner,
+        otc,
+        srcAssets: [sa1],
+      } = env;
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+      expect(await otc.assets(sa1.address)).to.equal(false);
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+      await expect(otc.connect(owner).addAsset(sa1.address))
+        .to.emit(otc, "AssetAdded")
+        .withArgs(sa1.address);
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+      expect(await otc.assets(sa1.address)).to.equal(true);
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("fail: not owner", async () => {
+      const {
+        accounts: [alice],
+        otc,
+        srcAssets: [, sa2],
+      } = env;
 
-        await time.increaseTo(unlockTime);
+      expect(await otc.assets(sa2.address)).to.equal(false);
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+      await expect(otc.connect(alice).addAsset(sa2.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+
+      expect(await otc.assets(sa2.address)).to.equal(false);
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("fail: asset already added", async () => {
+      const {
+        owner,
+        otc,
+        srcAssets: [sa1],
+      } = env;
 
-        await time.increaseTo(unlockTime);
+      expect(await otc.assets(sa1.address)).to.equal(true);
 
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+      await expect(otc.connect(owner).addAsset(sa1.address))
+        .to.be.revertedWithCustomError(otc, "OTCError")
+        .withArgs(Otc.ErrorType.AssetAlreadyAdded);
+
+      expect(await otc.assets(sa1.address)).to.equal(true);
     });
   });
 });
--- contract/test/types.ts
+++ contract/test/types.ts
@@ -1,10 +1,33 @@
 import { ERC20Mock, OTC } from "../typechain-types";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
 export interface TestEnv {
-  otc: OTC,
-  sourceAssets: ERC20Mock[],
-  destAssets: ERC20Mock[],
-  owner: SignerWithAddress,
-  accounts: SignerWithAddress[]
+  otc: OTC;
+  srcAssets: ERC20Mock[];
+  destAssets: ERC20Mock[];
+  owner: SignerWithAddress;
+  accounts: SignerWithAddress[];
+}
+
+export namespace Otc {
+  export enum ErrorType {
+    ZeroAddress,
+    SourceAssetUnregistered,
+    DestAssetUnregistered,
+    CommissionRateBeyondLimit,
+    AssetAlreadyAdded,
+    AssetAlreadyRemoved,
+  }
+}
+
+export namespace Offer {
+  export enum ErrorType {
+    InsufficientBalance,
+    OfferNotOpen,
+    OfferAccepted,
+    OfferNotAccepted,
+    NotCreator,
+    NotDomainOwner,
+    WithdrawLocked,
+  }
 }
