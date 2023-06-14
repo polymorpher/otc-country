aa334f5cd8f37e3cca2edc25ddabd81fd64b29a2
0xwormh0le <voyagergoldev@gmail.com>
2023-06-13 20:07:00 -0600

    update

--- app/src/components/NewOffer.tsx
+++ app/src/components/NewOffer.tsx
@@ -1,8 +1,7 @@
 import React, { useCallback } from 'react';
-import { Box, Text } from '@chakra-ui/react';
-import ethers, { BigNumber } from 'ethers';
-import { useAccount, useContractRead } from 'wagmi';
-import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input, Text } from '@chakra-ui/react';
+import * as ethers from 'ethers';
+import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi';
 import { readContract } from '@wagmi/core';
 import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
@@ -24,8 +23,16 @@ const checkAssetAvailable = (asset: string) =>
 const schema = (commissionRate: number) =>
   yup.object({
     domainOwner: yup.string().required().matches(regexEtherAddress),
-    srcAsset: yup.string().required().test('available', 'not available', checkAssetAvailable),
-    destAsset: yup.string().required().test('available', 'not available', checkAssetAvailable),
+    srcAsset: yup
+      .string()
+      .required()
+      .matches(regexEtherAddress)
+      .test('available', 'not available', checkAssetAvailable),
+    destAsset: yup
+      .string()
+      .required()
+      .matches(regexEtherAddress)
+      .test('available', 'not available', checkAssetAvailable),
     depositAmount: yup.number().required().min(0),
     acceptAmount: yup.number().required().min(0),
     commissionRate: yup.number().required().min(commissionRate),
@@ -40,8 +47,12 @@ const NewOffer: React.FC<NewOfferProps> = ({ domain }) => {
     functionName: 'commissionRateScale',
   });
 
-  const { register, handleSubmit } = useForm({
-    resolver: yupResolver(schema(BigNumber.from(commissionRateScale).toNumber())),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema(ethers.BigNumber.from(commissionRateScale).toNumber())),
   });
 
   const { config } = usePrepareContractWrite({
@@ -85,7 +96,50 @@ const NewOffer: React.FC<NewOfferProps> = ({ domain }) => {
   return (
     <Box>
       <Text>Please create a new offer with the following information.</Text>
-      <form onSubmit={handleSubmit(handleOfferSubmit)}></form>
+      <form onSubmit={handleSubmit(handleOfferSubmit)}>
+        <FormControl isInvalid={!!errors.domainOwner}>
+          <FormLabel>Domain owner</FormLabel>
+          <Input {...register('domainOwner')} />
+          <FormErrorMessage>{errors.domainOwner?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.srcAsset}>
+          <FormLabel>Source asset</FormLabel>
+          <Input {...register('srcAsset')} />
+          <FormErrorMessage>{errors.srcAsset?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.destAsset}>
+          <FormLabel>Destination asset</FormLabel>
+          <Input {...register('destAsset')} />
+          <FormErrorMessage>{errors.destAsset?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.depositAmount}>
+          <FormLabel>Deposit amount</FormLabel>
+          <Input {...register('depositAmount')} />
+          <FormErrorMessage>{errors.depositAmount?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.acceptAmount}>
+          <FormLabel>Accept amount</FormLabel>
+          <Input {...register('acceptAmount')} />
+          <FormErrorMessage>{errors.acceptAmount?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.commissionRate}>
+          <FormLabel>Commission rate</FormLabel>
+          <Input {...register('commissionRate')} />
+          <FormErrorMessage>{errors.commissionRate?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={!!errors.lockWithdrawDuration}>
+          <FormLabel>Lock withdraw duration</FormLabel>
+          <Input {...register('lockWithdrawDuration')} />
+          <FormErrorMessage>{errors.lockWithdrawDuration?.message}</FormErrorMessage>
+        </FormControl>
+        <Button type="submit">Create</Button>
+      </form>
     </Box>
   );
 };
