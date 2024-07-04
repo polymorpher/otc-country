b5261557c484bf507d2829bb029d69eaf68ee831
jwst9 <johnathonbaw@gmail.com>
2024-07-04 13:18:12 -0600

    remove commented code

--- app/src/components/DomainInput.tsx
+++ app/src/components/DomainInput.tsx
@@ -1,7 +1,5 @@
-import React, { useCallback, useMemo } from 'react'
+import React from 'react'
 import { Input, InputGroup, InputRightAddon, InputRightElement, Spinner } from '@chakra-ui/react'
-import debounce from 'lodash/debounce'
-import { debounceTimeout } from '~/helpers/config'
 
 interface DomainInputProps {
   onChange: (value: string) => void
@@ -9,21 +7,16 @@ interface DomainInputProps {
   value: string
 }
 
-const DomainInput: React.FC<DomainInputProps> = ({ onChange, loading, value }) => {
-  // const handleDebouncedChange = useMemo(() => debounce((e) => { onChange(e.target.value) }, debounceTimeout), [onChange])
-
-  return (
-    <InputGroup>
-      {/* <Input value={value} placeholder="my-offer" onChange={handleDebouncedChange} /> */}
-      <Input value={value} placeholder="my-offer" onChange={(e) => { onChange(e.target.value) }} />
-      {loading && (
-        <InputRightElement right="7.7em">
-          <Spinner />
-        </InputRightElement>
-      )}
-      <InputRightAddon>.country</InputRightAddon>
-    </InputGroup>
-  )
-}
+const DomainInput: React.FC<DomainInputProps> = ({ onChange, loading, value }) => (
+  <InputGroup>
+    <Input value={value} placeholder="my-offer" onChange={(e) => { onChange(e.target.value) }} />
+    {loading && (
+      <InputRightElement right="7.7em">
+        <Spinner />
+      </InputRightElement>
+    )}
+    <InputRightAddon>.country</InputRightAddon>
+  </InputGroup>
+)
 
 export default DomainInput
