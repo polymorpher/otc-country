c1fbea0e63a72083f0c564f5eeec2b3a2b4d79c1
polymorpher <aaron@hiddenstate.xyz>
2024-12-01 00:24:30 -0500

    stateful mock domain contract

--- contract/contracts/mocks/DomainContract.sol
+++ contract/contracts/mocks/DomainContract.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.9;
 import "../externals/IDC.sol";
 
 contract DomainContract is IDC {
+    mapping(bytes32 => address) public ownerLookup;
+
     function makeCommitment(string memory /*name*/, address /*owner*/, bytes32 /*secret*/) external pure override returns (bytes32) {
         return keccak256(abi.encodePacked("commit"));
     }
@@ -15,16 +17,18 @@ contract DomainContract is IDC {
      * https://github.com/polymorpher/dot-country/blob/main/contracts/contracts/DC.sol#L101
      */
     function available(string memory name) public pure returns (bool) {
-        return bytes(name).length > 3;
+        return bytes(name).length > 3 && ownerLookup[keccak256(abi.encodePacked(name))] == address(0);
     }
 
     function getPrice(string calldata /* name */) external pure override returns (uint256 price) {
         price = 54321;
     }
 
-    function ownerOf(string calldata /* name */) external pure returns (address owner) {
-        owner = address(0);
+    function ownerOf(string calldata name) external pure returns (address owner) {
+        return ownerLookup[keccak256(abi.encodePacked(name))];
     }
 
-    function register(string calldata name, address owner, bytes32 secret) external payable override {}
+    function register(string calldata name, address owner, bytes32 secret) external payable override {
+        ownerLookup[keccak256(abi.encodePacked(name))] = owner;
+    }
 }
