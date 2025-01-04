477e54bdf8713e1df2465c37b71fed492a8388c1
polymorpher <aaron@hiddenstate.xyz>
2025-01-04 14:02:00 -0800

    fix deploy script

--- contract/setup-anvil.sh
+++ contract/setup-anvil.sh
@@ -9,8 +9,8 @@ export USER=$(cast wallet address --private-key ${PRIVATE_KEY})
 cast nonce ${DEPLOYER_ADDRESS}
 #npx hardhat deploy --network anvil --reset
 
-cast send ${TOKEN_A} "mint(address,uint256)" ${USER} 12000000000000000000 --private-key ${PRIVATE_KEY}
+cast send ${TOKEN_A} "mint(address,uint256)" ${USER} 12000000000000000000 --private-key ${PRIVATE_KEY} --legacy
 
-cast send ${OTC} "addAsset(address)" ${TOKEN_A} --private-key ${ADMIN_PRIVATE_KEY}
-cast send ${OTC} "addAsset(address)" ${TOKEN_B} --private-key ${ADMIN_PRIVATE_KEY}
+cast send ${OTC} "addAsset(address)" ${TOKEN_A} --private-key ${ADMIN_PRIVATE_KEY} --legacy
+cast send ${OTC} "addAsset(address)" ${TOKEN_B} --private-key ${ADMIN_PRIVATE_KEY} --legacy
 
