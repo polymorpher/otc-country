867ac43e73520930cbffc00dd2c43e18ea5e2f02
polymorpher <aaron@hiddenstate.xyz>
2025-01-07 17:12:39 -0800

    mint token script

---
+++ contract/mint-token.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+set -x
+
+export $(grep -v '^#' .env.script | xargs)
+export $(grep -v '^#' .env.mint | xargs)
+
+cast send ${TOKEN_A} "mint(address,uint256)" ${TARGET_USER} 12000000000000000000 --private-key ${PRIVATE_KEY} --legacy
+cast send ${TOKEN_B} "mint(address,uint256)" ${TARGET_USER} 12000000000000000000 --private-key ${PRIVATE_KEY} --legacy
+
